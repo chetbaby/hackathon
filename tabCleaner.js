1b25352488d8a15a150cd745292f9cff95c98a1a
chetbaby <cbaby@CBMB.local>
2019-04-04 00:27:00 -0700

    close all unpinned tabs works

--- tabCleaner.js
+++ tabCleaner.js
@@ -1,20 +1,27 @@
 chrome.runtime.onInstalled.addListener(function() {
-  /* chrome.storage.sync.set({ color: "#3aa757" } */
   console.log("The color is green.");
 });
 
-// console.log(chrome.commands);
-// console.log(chrome.runtime);
-// console.log(chrome.browserAction);
-// console.log(chrome);
-// console.log(chrome);
-// console.log(chrome.commands);
-// console.log(chrome.commands.getAll());
 chrome.commands.onCommand.addListener(command => {
   if (command === "pin-tab") {
     console.log("pin tab works!");
     chrome.tabs.query({ active: true }, function(tab) {
-      chrome.tabs.update(tab[1].id, { pinned: true });
+      chrome.tabs.update(tab[0].id, { pinned: true });
+    });
+  }
+
+  if (command === "close-tabs") {
+    chrome.tabs.query({ pinned: false }, function(tab) {
+      tab.forEach(function(el) {
+        chrome.tabs.remove(el.id);
+      });
     });
   }
 });
+
+let closeUnpinnedTabs = {
+  id: "CloseUnpinnedTabs",
+  title: "CloseUnpinnedTabs",
+  contexts: ["all"]
+};
+chrome.contextMenus.create(closeUnpinnedTabs);
