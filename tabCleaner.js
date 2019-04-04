7571738de5b4b817fcb03ae34f6272d0d50d5185
chetbaby <cbaby@CBMB.local>
2019-04-04 15:02:17 -0700

    just fixed pin-all

--- tabCleaner.js
+++ tabCleaner.js
@@ -2,10 +2,13 @@ chrome.runtime.onInstalled.addListener(function() {
   console.log("The color is green.");
 });
 
+let allPinned = false;
+
 chrome.commands.onCommand.addListener(command => {
   if (command === "pin-tab") {
     console.log("pin tab works!");
-    chrome.tabs.query({ active: true }, function(tab) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tab) {
+      console.log(tab);
       chrome.tabs.update(tab[0].id, { pinned: true });
     });
   }
@@ -17,11 +20,41 @@ chrome.commands.onCommand.addListener(command => {
       });
     });
   }
+
+  if (command === "(un)pin-all") {
+    console.log("hi");
+    chrome.tabs.query({ currentWindow: true }, function(tab) {
+      tab.forEach(function(el) {
+        chrome.tabs.update(el.id, { pinned: true });
+      });
+    });
+  }
+  //TODO: insert unpin-all
+
+  // if (command === "unpin-all") {
+  //   console.log("hi");
+  //   chrome.tabs.query({ currentWindow: true }, function (tab) {
+
+  //     tab.forEach(function(el) {
+  //       chrome.tabs.update(el.id, { pinned: true });
+  //     });
+  //   });
+  // }
 });
 
 let closeUnpinnedTabs = {
-  id: "CloseUnpinnedTabs",
-  title: "CloseUnpinnedTabs",
+  id: "closeUnpinnedTabs",
+  title: "closeUnpinnedTabs",
   contexts: ["all"]
 };
 chrome.contextMenus.create(closeUnpinnedTabs);
+
+chrome.contextMenus.onClicked.addListener(function(clicked) {
+  if (clicked.menuItemId === "closeUnpinnedTabs") {
+    chrome.tabs.query({ pinned: false }, function(tab) {
+      tab.forEach(function(el) {
+        chrome.tabs.remove(el.id);
+      });
+    });
+  }
+});
