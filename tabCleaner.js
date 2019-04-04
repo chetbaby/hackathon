373b8aed55b8d2ee33a201e6e0f02efb83c5a1be
chetbaby <cbaby@CBMB.local>
2019-04-03 22:43:28 -0700

    got the shortcut key working

--- tabCleaner.js
+++ tabCleaner.js
@@ -3,12 +3,14 @@ chrome.runtime.onInstalled.addListener(function() {
   console.log("The color is green.");
 });
 
-console.log(chrome.commands);
+// console.log(chrome.commands);
 // console.log(chrome.runtime);
 // console.log(chrome.browserAction);
 // console.log(chrome);
 // console.log(chrome);
 // console.log(chrome.commands);
-chrome.commands.onCommand.addListener(function(command) {
-  console.log("hi");
-});
+// console.log(chrome.commands.getAll());
+chrome.commands.onCommand.addListener(command => console.log(command));
+// chrome.commands.onCommand.addListener(function(command) {
+// console.log("hi");
+// });
